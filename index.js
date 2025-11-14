66f341987aa2830b309dee2db5930d997bba3c4d
SaiHimaSindhu <sindhukancherla2003@gmail.com>
2025-11-14 15:46:33 +0530

    Added login function

--- index.js
+++ index.js
@@ -10,3 +10,20 @@ function signup(userName){
         return "Signup_Successful,_Please_Login";
     }
 }
+
+function login(userName,password){
+    let users=['sindhu','rajeev','pavani','ramana'];
+    let correctPassword='Emp@123';
+    if(!users.includes(userName))
+    {
+        return "User_Not_Found,_Please_Signup";
+    }
+    else if(password === correctPassword)
+    {
+        return "Login_Successful";
+    }
+    else
+    {
+        return "Wrong_Password";
+    }
+}
